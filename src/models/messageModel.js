5ac3011c14fa022b417210c91f53aff019733244
William Lima <williamlima@natura.net>
2021-02-25 23:10:27 -0300

    add uuid for conversationId

--- src/models/messageModel.js
+++ src/models/messageModel.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 const MessageSchema = mongoose.Schema(
   {
     id: {type: String, unique : true,default: uuidv4, index: true},
-    conversationId: {type: String},
+    conversationId: {type: String, default: uuidv4},
     timestamp: { type: Date, default: Date.now },
     from: {type: String},
     to: {type: String},
